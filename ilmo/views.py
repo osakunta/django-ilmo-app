6066f248f6b738137d91e01ea901b310fb37f80a
Matti Remes <matti.remes@eficode.com>
2016-07-20 19:48:42 +0300

    list events view

--- ilmo/views.py
+++ ilmo/views.py
@@ -1,3 +1,15 @@
+from django.template.loader import get_template
+from django.template import Context
 from django.shortcuts import render
+from ilmo.models import Event
+from django.http import HttpResponse
 
+def get_all_events(request):
+    events = Event.objects.all()
+    t = get_template('list.html')
+    html = t.render(Context({'event_list' : events, 'page_title' : 'Ilmo'}))
+    return HttpResponse(html)
+def index(request):
+    t = get_template('list.html')
+    return HttpResponse("Hello world")
 # Create your views here.
