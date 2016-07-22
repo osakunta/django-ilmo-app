3f61f81467cf817f127181301926c5d4c086baf4
Matti Remes <matti.remes@eficode.com>
2016-07-22 16:08:53 +0300

    new admin model

--- ilmo/admin.py
+++ ilmo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Event,EventAttendee
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name','event_date','close_date')
+class EventAttendeeAdmin(admin.ModelAdmin):
+    list_display = ('event','attendee_name','registration_date')
+
+admin.site.register(Event,EventAdmin)
+admin.site.register(EventAttendee,EventAttendeeAdmin)
