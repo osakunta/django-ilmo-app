3c915511aafc047b305ac0bcec4f2f53044ffcce
Matti Remes <matti.remes@eficode.com>
2016-07-29 16:55:07 +0300

    new structure

--- ilmo-app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_PATH = os.path.dirname(__file__)
-RESOURCE_PATH = BASE_PATH + '/resources/'
-FORM_TEMPLATE_PATH = BASE_PATH + '/templates/form/'
-EMAIL_TEMPLATE_PATH= BASE_PATH + '/templates/email/'
-
-EMAIL_CONFIGURED = False
