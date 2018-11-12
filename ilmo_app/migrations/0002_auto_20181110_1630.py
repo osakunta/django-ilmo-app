617d5a1f9e6c384455d93ba164b63ab249a0f92a
Matti Remes <remes@iki.fi>
2018-11-12 21:35:19 +0200

    Fix migration of the foreign key field

--- ilmo_app/migrations/0002_auto_20181110_1630.py
+++ ilmo_app/migrations/0002_auto_20181110_1630.py
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='event',
             name='form',
-            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, to='ilmo_app.EventForm'),
+            field=models.ForeignKey(default=None, null=True, on_delete=django.db.models.deletion.CASCADE, to='ilmo_app.EventForm'),
             preserve_default=False,
         ),
     ]
