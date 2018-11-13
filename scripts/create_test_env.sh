9b2027550c3a1fe91314e369f8b4159aacb9eb62
Matti Remes <remes@iki.fi>
2018-11-13 21:38:12 +0200

    Another attempt to fix CI

--- scripts/create_test_env.sh
+++ scripts/create_test_env.sh
@@ -5,8 +5,11 @@ source scripts/env.sh
 
 echo "Creating test environment"
 
-echo "Installing requirements..."
-pip install -r requirements.txt
+if [ "$1" == "--install-requirements" ];
+then
+    echo "Installing requirements..."
+    pip install -r requirements.txt
+fi
 
 echo "Creating test site"
 sitename=testsite
@@ -15,7 +18,20 @@ cd ${sitename}
 ln -s ../ilmo_app ilmo_app
 
 settings_file=${sitename}/settings.py
-sed -i .bak 's/INSTALLED_APPS =.*/&"ilmo_app",/' ${settings_file}
-rm ${settings_file}.bak
+
+pattern="s/INSTALLED_APPS =.*/&'ilmo_app',/"
+
+case "$(uname -s)" in
+  Darwin)
+      sed -i .bak "${pattern}" ${settings_file}
+      rm ${settings_file}.bak
+      ;;
+  Linux)
+      sed -i "${pattern}" ${settings_file}
+      ;;
+  *)
+     exit 1
+     ;;
+esac
 
 echo "Done"
