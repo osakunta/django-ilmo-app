8901257c22c37743ed5479da4b4cbf142028752b
Matti Remes <remes@iki.fi>
2019-11-10 22:57:20 +0200

    Add tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from ilmo_app.models import Event, EventAttendee, Place, Payment
 from ilmo_app.models.exceptions import EventCannotAttendException, InvalidPaymentMethod
-from datetime import date, timedelta
+from datetime import timedelta
 from django.utils import timezone
 import uuid
+import json
 
 
 class TestPaymentModel(TestCase):
@@ -79,38 +80,55 @@ class TestEvent(TestCase):
 
 
 class EventAttendeeTestCase(TestCase):
+    def setUp(self):
+        _create_events()
+
+    def test_event_attendee_gets_base_price(self):
+        payment = Payment(method='Muu', price=10)
+        payment.save()
+        event = Event.objects.get(name='EventInFuture')
+        event.payment = payment
+        event.save()
+
+        attendee = EventAttendee(attendee_name='Foo Bar',
+                                 event=event,
+                                 registration_date=timezone.now())
+        attendee.save()
+        self.assertEquals(payment.price, attendee.get_price())
+
+    def test_event_attendee_gets_discount_based_on_model_detail_field(self):
+        discount = -2
+        payment = Payment(method='Muu', price=10, special_price_offsets=json.dumps(dict(age_group=dict(youth=discount))))
+        payment.save()
+        event = Event.objects.get(name='EventInFuture')
+        event.payment = payment
+        event.save()
+
+        attendee = EventAttendee(attendee_name='Foo',
+                                 event=event,
+                                 registration_date=timezone.now(),
+                                 attendee_details=json.dumps(dict(age_group="youth")))
+        attendee.save()
+        self.assertEquals(payment.price + discount, attendee.get_price())
 
 
 def _create_events():
     place = {'name': 'Place'}
+
+    future_dt = timezone.now() + timedelta(days=1)
+    past_dt = timezone.now() - timedelta(days=1)
+
     events = [
-        {'name': 'EventInPast', 'event_date': timezone.now() - timedelta(minutes=1), 'capacity': 1,
-         'close_date': timezone.now() - timedelta(days=1), 'description': 'empty'},
-        {'name': 'EventWithZeroCapacity', 'event_date': date(2015, 1, 1), 'capacity': 0,
-         'close_date': timezone.now() + timedelta(days=1), 'description': 'empty', 'backup': False},
-        {'name': 'Event2', 'event_date': date(2015, 1, 1), 'capacity': 1,
-         'close_date': timezone.now() + timedelta(days=1), 'description': 'empty'},
-        {'name': 'Event3', 'event_date': date(2015, 1, 1), 'capacity': 1,
-         'close_date': timezone.now() + timedelta(days=1), 'description': 'empty'},
-        {'name': 'EventInFuture', 'event_date': timezone.now() + timedelta(hours=1), 'capacity': 1,
-         'close_date': timezone.now() + timedelta(days=1), 'description': 'empty'},
-        {'name': 'EventWithOpenDateInFuture',
-         'event_date': timezone.now() + timedelta(days=1),
-         'capacity': 1,
-         'close_date': timezone.now() + timedelta(days=1),
-         'description': 'empty',
-         'open_date': timezone.now() + timedelta(days=1)},
-        {'name': 'EventWithPastOpenDate',
-         'event_date': timezone.now() + timedelta(days=1),
-         'capacity': 1,
-         'close_date': timezone.now() + timedelta(days=1),
-         'description': 'empty',
-         'open_date': timezone.now() - timedelta(days=1)}
+        {'name': 'EventInPast', 'event_date': past_dt, 'capacity': 1, 'close_date': past_dt},
+        {'name': 'EventWithZeroCapacity', 'event_date': future_dt, 'capacity': 0, 'close_date': future_dt, 'backup': False},
+        {'name': 'EventInFuture', 'event_date': future_dt, 'capacity': 1, 'close_date': future_dt},
+        {'name': 'EventWithOpenDateInFuture', 'event_date': future_dt, 'capacity': 1, 'close_date': future_dt, 'open_date': future_dt},
+        {'name': 'EventWithPastOpenDate', 'event_date': future_dt, 'capacity': 1, 'close_date': future_dt, 'open_date': past_dt}
     ]
 
     place = Place.objects.create(**place)
     for event in events:
-        Event.objects.create(**event, url_alias=uuid.uuid4(), place=place)
+        Event.objects.create(**event, description='foo', url_alias=uuid.uuid4(), place=place)
 
 
 def _attend_event(event, times=1):
